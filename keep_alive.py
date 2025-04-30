e05cb8a811f900d75f86b12b5167fdb7e3f658fb
abdo254aa <abdelillahjaoudate2004@gmail.com>
2025-04-30 11:42:12 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -44,7 +44,7 @@ def self_ping():
 
         try:
 
-            requests.get("https://aliexpress-telegram-bot-owz5.onrender.com")  
+            requests.get("https://aliexpress-telegram-bot-177z.onrender.com")  
 
             print(" hello world ")
 
